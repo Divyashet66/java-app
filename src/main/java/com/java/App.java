60e42c5389b68d68ea05e8cea29e178c4ca566bd
Divya Shet <88928587+Divyashet66@users.noreply.github.com>
2022-11-28 11:53:13 +0530

    Add files via upload

---
+++ src/main/java/com/java/App.java
@@ -0,0 +1,13 @@
+package com.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to buildpacks!..Build java images without Dockerfile :)" );
+    }
+}
